7a9cc85bc4a0851acf2ceaa77386c0542644533e
Felmeta Muktar <felmetamuktar23@gmail.com>
2024-09-25 19:41:28 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
+
+const app = express();
+dotenv.config();
+
+
+// Connect to MongoDB
+mongoose
+.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log('Failed to connect to MongoDB', err);
+});
+
+app.use(express.json())
+app.use("/api/auth",authRoute);
+app.use("/api/users",userRoute);
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+})
