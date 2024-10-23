940eefadd2e84abcddfa2c109cf9fc8f3db34592
Felmeta Muktar <felmetamuktar23@gmail.com>
2024-10-23 15:57:53 +0300

    delete user

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const {verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin} = require(
 const router = require("express").Router();
 
 
+// Update method
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     if(req.body.password) {
         req.body.password = CryptoJS.AES.encrypt(
@@ -21,17 +22,17 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     }
 })
 
-//Delete
+//Delete method
 router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
     try {
         await User.findByIdAndDelete(req.params.id)
-        res.status(200).json("User has been deleted...");
+        res.status(200).json("User has been deleted!!!");
     } catch (error) {
         res.status(500).json(error);
     }
 })
 
-//Get user
+//Get user method
 router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
     try {
         const user = await User.findById(req.params.id);
