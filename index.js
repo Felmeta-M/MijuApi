e0e0b9e9e9373bb234def81d9b715626e6e6d7ab
Felmeta Muktar <felmetamuktar23@gmail.com>
2024-10-25 23:09:05 +0300

    xommit

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
+const stripeRoute = require('./routes/stripe');
+const chapaRoute = require('./routes/chapa');
+const cors = require('cors');
 
 
 const app = express();
@@ -21,12 +24,15 @@ mongoose
     console.log('Failed to connect to MongoDB', err);
 });
 
+app.use(cors());
 app.use(express.json())
 app.use("/api/auth",authRoute);
 app.use("/api/users",userRoute);
 app.use("/api/products",productRoute);
 app.use("/api/carts",cartRoute);
 app.use("/api/orders",orderRoute);
+app.use("/api/checkout",stripeRoute);
+app.use("/api/checkout", chapaRoute);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
